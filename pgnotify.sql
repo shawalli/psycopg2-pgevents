f07e9db8093b5ac4471022cf3a07dd3978cfb1db
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-08-10 18:48:44 -0400

    notify with json

--- pgnotify.sql
+++ pgnotify.sql
@@ -2,9 +2,14 @@ CREATE OR REPLACE FUNCTION pgnotify()
 RETURNS TRIGGER AS $function$
   BEGIN
     RAISE WARNING 'triggered';
-    NOTIFY pgnotify, 'halp1';
-    NOTIFY PGNOTIFY, 'halp2';
-    NOTIFY "pgnotify", 'halp3';
+    PERFORM pg_notify(
+        'pgnotify',
+        json_build_object(
+          'schema_name', TG_TABLE_SCHEMA,
+          'table_name', TG_TABLE_NAME,
+          'id', NEW.id
+      )::text
+    );
     RETURN NEW;
   END;
 $function$
