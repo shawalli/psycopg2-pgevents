9b1fd5bd058864db3caf67b9102ca1f26bc7ea07
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-08-16 02:04:04 -0400

    adds test for removing trigger function

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -1,16 +1,22 @@
+from psycopg2 import DatabaseError, ProgrammingError
+from pytest import fixture, mark
 
 from sqlalchemy_pgevents.base import execute
-from sqlalchemy_pgevents.trigger import register_trigger_function
+from sqlalchemy_pgevents.trigger import register_trigger_function, unregister_trigger_function
+
+
+@fixture
+def trigger_fn_registered(connection):
+    register_trigger_function(connection)
 
 
 class TestTrigger:
     def test_add_trigger_function(self, connection):
-        register_trigger_function(connection)
-
         trigger_function_registered = False
+
+        register_trigger_function(connection)
         try:
-            execute(
-                connection, "SELECT pg_get_functiondef('public.pgevents'::regproc);")
+            execute(connection, "SELECT pg_get_functiondef('public.pgevents'::regproc);")
             trigger_function_registered = True
         except DatabaseError:
             # Ignore error, its only use in this test is cause following
@@ -18,3 +24,15 @@ class TestTrigger:
             pass
 
         assert (trigger_function_registered == True)
+
+    @mark.usefixtures('trigger_fn_registered')
+    def test_remove_trigger_function(self, connection):
+        trigger_function_registered = True
+
+        unregister_trigger_function(connection)
+        try:
+            execute(connection, "SELECT pg_get_functiondef('public.pgevents'::regproc);")
+        except ProgrammingError:
+            trigger_function_registered = False
+
+        assert (trigger_function_registered == False)
