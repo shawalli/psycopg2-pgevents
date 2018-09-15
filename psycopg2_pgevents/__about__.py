0b36f360de6df2065ca5b35b72f2be016dd69b47
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-09-15 12:16:37 -0400

    changes version number to 0.1.1

--- psycopg2_pgevents/__about__.py
+++ psycopg2_pgevents/__about__.py
@@ -3,7 +3,7 @@ __title__ = 'psycopg2-pgevents'
 __summary__ = 'PostGreSQL LISTEN/NOTIFY functionality, via psycopg2.'
 __uri__ = 'https://github.com/shawalli/psycopg2-pgevents'
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __author__ = 'Shawn Wallis'
 __email__ = 'shawn.p.wallis@gmail.com'
